887dc24243798301b05ee0c58c41b9768982129c
Manoj Modhwadia <manojmodhwadia@outlook.com>
2020-05-23 16:50:17 +0100

    project initialised with webpack and babel - styles not working yet

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  // mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  // devtool: process.env.NODE_ENV === 'production' ? 'source-map' : 'source-map',
+  entry: './index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    port: 9000,
+    // publicPath: "http://localhost:9000/dist/",
+    hotOnly: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+};
